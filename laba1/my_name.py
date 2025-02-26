d6c50201a823da65a7f439e2b16408130c2b1a0a
Lol547 <sokolovkirill489@gmail.com>
2025-02-26 14:58:11 +0300

    Объединены три коммита

--- laba1/my_name.py
+++ laba1/my_name.py
@@ -1,15 +1,14 @@
 class Calculator:
 
-    def __init__(self, num_1: int, num_2: int) -> None:
+    def __init__(self, num_1: [int, float], num_2: [int, float]) -> None:
         self.num_1 = num_1
         self.num_2 = num_2
 
-    def plus(self) -> int:
+    def plus(self) -> [int, float]:
         return self.num_1 + self.num_2
 
-    def multiply(self) -> int:
+    def multiply(self) -> [int, float]:
         return self.num_1 * self.num_2
 
-
-test = Calculator(2, 2)
-print(test.plus(), test.multiply())
+    def pow(self) -> [int, float]:
+        return self.num_1 ** self.num_2
